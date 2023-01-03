7f1faff46394dce55de0e612b07129ef6d60ad66
Eugeny Volobuev <qulert@gmail.com>
2023-01-03 19:55:38 +0700

    sync

--- clipr-cli/src/main.rs
+++ clipr-cli/src/main.rs
@@ -1,33 +1,30 @@
 use anyhow::{bail, Result};
-use async_std::task;
 use clap::Parser;
 use clipr_common::{Args, Command, Config, Payload};
 use std::sync::Arc;
 
 async fn call(config: Arc<Config>, cmd: Command) -> Result<Payload, surf::Error> {
-    let connect_to = format!(
-        "{}:{}",
+    let uri = format!(
+        "http://{}:{}/command",
         &config.host.as_ref().unwrap(),
         &config.json_port.unwrap()
     );
-    let uri = format!("http://{}/command", connect_to);
     let req = surf::post(uri).body_json(&cmd)?;
     let rep: Payload = req.recv_json().await?;
     Ok(rep)
 }
 
-fn main() -> Result<()> {
+#[async_std::main]
+async fn main() -> Result<()> {
     let args = Args::parse();
     let config = Arc::new(Config::load_from_args(&args)?);
 
-    match args.command {
-        Some(cmd) => match task::block_on(call(config, cmd)) {
-            Ok(payload) => {
-                println!("{}", String::from(&payload));
-                Ok(())
-            }
+    if let Some(cmd) = args.command {
+        match call(config, cmd).await {
+            Ok(payload) => println!("{}", String::from(&payload)),
             Err(err) => bail!(err),
-        },
-        None => Ok(()),
+        }
     }
+
+    Ok(())
 }
