6b3a9980b4d377bb9e8e035eb7866199a3e63347
Eugeny Volobuev <qulert@gmail.com>
2025-07-20 22:38:58 +0800

    cleanup deps

--- clipr-daemon/src/http.rs
+++ clipr-daemon/src/http.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use socket2::{Domain, Socket, Type};
-use std::io::{BufReader, prelude::*};
+use std::io::{prelude::*, BufReader};
 use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::sync::mpsc::Sender;
 
@@ -19,36 +19,13 @@ pub fn server(listen_on: String, sender: Sender<clipr_common::Request>) -> Resul
     Ok(())
 }
 
-// fn client(listen_on: &str, cmd: Command) -> Result<Payload> {
-//     let socket = Socket::new(Domain::IPV4, Type::STREAM, None)?;
-//     socket.set_reuse_port(true)?;
-//     socket.set_reuse_address(true)?;
-//     let address: SocketAddr = listen_on.parse()?;
-//     socket.connect(&address.into())?;
-//     let stream: TcpStream = socket.into();
-//     let cmd_body = serde_json::to_string(&cmd)?;
-//     stream.write_all("POST /command HTTP/1.1\r\n\r\n".as_bytes())?;
-//     stream.write_all(cmd_body.as_bytes())?;
-//     stream.flush()?;
-//     // stream.read_to_string
-//     // TODO:
-//     // +1. serialize command
-//     // +2. write data
-//     // +3. flush
-//     // 4. read response
-//     // 5. deserialize response
-// }
-
 fn handle_connection(sender: Sender<clipr_common::Request>, mut stream: &TcpStream) -> Result<()> {
+    let mut buffer = [0; 256];
     let mut buf_reader = BufReader::new(&mut stream);
-    // TODO: fix it
-    let mut buffer = [0; 1024];
     let size = buf_reader.read(&mut buffer)?;
-    let mut headers = [httparse::EMPTY_HEADER; 16];
+    let mut headers = [httparse::EMPTY_HEADER; 4];
     let mut req = httparse::Request::new(&mut headers);
     let offset = req.parse(&buffer[..size])?.unwrap();
-    // TODO: add content-length assertion
-    let _content_length_pre = size - offset;
     let response = match (req.method, req.path) {
         (Some("POST"), Some("/command")) => {
             let content_length: usize = String::from_utf8_lossy(
@@ -59,9 +36,14 @@ fn handle_connection(sender: Sender<clipr_common::Request>, mut stream: &TcpStre
                     .value,
             )
             .parse()?;
-            // TODO: content_length_pre == content_length
-            let body = &buffer[offset..];
-            let cmd: clipr_common::Command = serde_json::from_slice(&body[..content_length])?;
+            let read_body_bytes = size - offset;
+            let mut body: Vec<u8> = Vec::from(&buffer[offset..offset + read_body_bytes]);
+            if read_body_bytes < content_length {
+                let mut rest: Vec<u8> = vec![0; content_length - read_body_bytes];
+                buf_reader.read_exact(&mut rest)?;
+                body.append(&mut rest);
+            }
+            let cmd: clipr_common::Command = serde_json::from_slice(&body)?;
             let rep_body = match clipr_common::Request::send_cmd(&sender, cmd) {
                 Some(clipr_common::Response::Payload(val)) => serde_json::to_string(&val)?,
                 _ => String::from("{}"),
